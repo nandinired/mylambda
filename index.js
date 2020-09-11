ab6811684922dc9511322aa5ea6394a619ebc2e0
Nandini Baddam <nand8468@md33vhml7j.home>
2020-09-10 20:07:34 -0700

    aa

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The timein in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time format in in Los Angeles is: ' + currentTime.toString(),
     });
 };
