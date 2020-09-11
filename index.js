56f0aa39afe593decf57174be3b3fc82d0009e8a
Nandini Baddam <nand8468@md33vhml7j.home>
2020-09-10 17:07:27 -0700

    add

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-var time = require('time');
-exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date();
-    currentTime.setTimezone("America/Los_Angeles");
-    callback(null, {
-        statusCode: '200',
-        body: 'The time in the Los Angeles is: ' + currentTime.toString(),
+var exec = require('child_process').exec;
+
+var result = function(command, cb){
+    var child = exec(command, function(err, stdout, stderr){
+        if(err != null){
+            return cb(new Error(err), null);
+        }else if(typeof(stderr) != "string"){
+            return cb(new Error(stderr), null);
+        }else{
+            return cb(null, stdout);
+        }
     });
-};
+}
+
+exports.result = result;
+
+#var time = require('time');
+#exports.handler = (event, context, callback) => {
+#    var currentTime = new time.Date();
+#    currentTime.setTimezone("America/Los_Angeles");
+#    callback(null, {
+#        statusCode: '200',
+#        body: 'The time in the Los Angeles is: ' + currentTime.toString(),
+#    });
+#};
