1e457748ef083d73cc56c5fdac8204b08df6e960
Nandini Baddam <nand8468@md33vhml7j.home>
2020-09-10 17:11:51 -0700

    upda

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-var exec = require('child_process').exec;
-
-var result = function(command, cb){
-    var child = exec(command, function(err, stdout, stderr){
-        if(err != null){
-            return cb(new Error(err), null);
-        }else if(typeof(stderr) != "string"){
-            return cb(new Error(stderr), null);
-        }else{
-            return cb(null, stdout);
-        }
-=======
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
@@ -18,11 +5,9 @@ exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: '200',
         body: 'The time in the Los Angeles now is: ' + currentTime.toString(),
->>>>>>> 5e06b16293712ce426edadadb6f8e7297ce83f9b
     });
-}
+};
 
-exports.result = result;
 
 #var time = require('time');
 #exports.handler = (event, context, callback) => {
